f9d9fda779590d396783d4785b39b5382efa19f1
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2024-02-09 11:27:41 +0100

    Try qasync custom event loop
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- MainWindow/__init__.py
+++ MainWindow/__init__.py
@@ -1,17 +1,18 @@
 from PySide6.QtWidgets import QMainWindow, QPushButton
-
+from ipykernel.kernelapp import IPKernelApp
 from QtConsoleSpawner import new_qt_console
 
 
 class MainWindow(QMainWindow):
-    def __init__(self, kernel):
+    def __init__(self, kernel=None):
         super().__init__()
         self._kernel = kernel
         self._console = None
         self._button = QPushButton("Start QtConsole")
         self._button.clicked.connect(self.start_qt_console)
         self.setCentralWidget(self._button)
-        self._kernel.shell.push({"main_window": self})
+        if self._kernel is not None:
+            self._kernel.shell.push({"main_window": self})
 
     def closeEvent(self, event):
         event.accept()
@@ -23,4 +24,11 @@ class MainWindow(QMainWindow):
         super().closeEvent(event)
 
     def start_qt_console(self):
-        self._console = new_qt_console(self._kernel)
+        if self._kernel is None:
+            self._kernel = IPKernelApp.instance(kernel_name="SciQLop")
+            self._kernel.initialize(["python", "--gui=qt6", "--colors=linux"])
+            self._kernel.shell.push({"main_window": self})
+            self._console = new_qt_console(self._kernel)
+            self._kernel.start()
+        else:
+            self._console = new_qt_console(self._kernel)
--- QAsync/__init__.py
+++ QAsync/__init__.py
@@ -1,29 +1,55 @@
 from qasync import QEventLoop, QApplication
+from PySide6.QtCore import QTimer
+from typing import Optional
 import asyncio
 
 from MainWindow import MainWindow
 import sys
 
 from ipykernel.kernelapp import IPKernelApp
+from ipykernel.ipkernel import IPythonKernel
+from ipykernel.eventloops import register_integration, enable_gui
+
+event_loop: Optional[QEventLoop] = None
+app: Optional[QApplication] = None
+
+
+def get_app() -> QApplication:
+    global app
+    if app is None:
+        app = QApplication(sys.argv)
+    return app
+
+
+def get_event_loop() -> QEventLoop:
+    global event_loop
+    if event_loop is None:
+        event_loop = QEventLoop(get_app())
+        asyncio.set_event_loop(event_loop)
+    return event_loop
+
+
+@register_integration('my_loop')
+def loop_sciqlop(kernel: IPythonKernel):
+    """Start the SciQLop event loop."""
+    timer = QTimer()
+    timer.timeout.connect(kernel.do_one_iteration)
+    timer.start(int(kernel._poll_interval))
+    get_event_loop().exec()
 
 
 def main():
-    app = QApplication(sys.argv)
-
-    event_loop = QEventLoop(app)
-    asyncio.set_event_loop(event_loop)
-
+    event_loop = get_event_loop()
+    app = get_app()
     app_close_event = asyncio.Event()
     app.aboutToQuit.connect(app_close_event.set)
-    kernel = IPKernelApp.instance(kernel_name="SciQLop")
-    kernel.initialize(["python", "--gui=qt6", "--colors=linux"])
-    main_window = MainWindow(kernel)
+    kernel_app = IPKernelApp.instance(kernel_name="MyKernel")
+    kernel_app.eventloop = None
+    enable_gui('my_loop', kernel=kernel_app)
+    kernel_app.initialize()
+    main_window = MainWindow(kernel=kernel_app)
     main_window.show()
-    # this start function is blocking :/ but it still works
-    kernel.start()
-    #with event_loop:
-    #    print("Starting event loop")
-    #    event_loop.run_until_complete(app_close_event.wait())
+    kernel_app.start()
 
 
 if __name__ == "__main__":
